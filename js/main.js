70009749f97b108044642325b274835be704a45b
Gino Ghalehpardaz <ginoghalehpardaz@Ginos-MacBook-Pro-2.local>
2019-07-10 23:33:58 -0400

    added some JS and still working around some bugs

--- js/main.js
+++ js/main.js
@@ -1,13 +1,42 @@
-function allowDrop(ev){
-  ev.preventDefault();
-}
+console.log('fired!');
 
-function drag(ev){
-  ev.dataTransfer.setData("icon",ev.target.id);
-}
 
-function drop(ev){
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("icon");
-  ev.target.appendChild(document.getElementById(data));
-}
+ dropZones = document.querySelectorAll('play_board');
+
+ function allowDrop(ev){
+   ev.preventDefault();
+ }
+
+ function drag(ev){
+   ev.dataTransfer.setData("icon",ev.target.id);
+ }
+
+ function drop(ev){
+   ev.preventDefault();
+   var data = ev.dataTransfer.getData("icon");
+   ev.target.appendChild(document.getElementById(data));
+
+
+
+ dropZones.forEach(zone => {
+ zone.addEventListener("play_board", function(e) {
+ e.preventDefault();
+ console.log('dragged something over me');
+
+ if (this.childElementCount == 1) {return; }
+ console.log('one child family, go away!');
+});
+
+
+ zone.addEventListener("drop", function(e){
+ e.preventDefault(); // dont do your default behaviour instead do the following (console log)
+ console.log('you dropped something over me');
+
+ if (this.childElementCount == 1) {return; }
+ console.log('one child family, go away!');
+
+
+
+});
+});
+ }
