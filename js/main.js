016c3dbe71f451f93ac03561bbbd8741d1a4f4f1
Gino Ghalehpardaz <ginoghalehpardaz@Ginos-MBP-2.fc.ca>
2019-08-06 13:23:03 -0400

    cleaning up JS code

--- js/main.js
+++ js/main.js
@@ -1,64 +1,62 @@
+(() =>{
+
 console.log('fired!');
 
  icon_holder = document.querySelectorAll('icon_holder');
  drag_Box = document.querySelectorAll('#drag_Box');
- dropZones = document.querySelectorAll('play_board');
+ dropZones = document.querySelectorAll('.drag_Box');
 
  function allowDrop(ev){
    ev.preventDefault();
  }
 
  function drag(ev){
-   ev.dataTransfer.setData("icon",ev.target.id);
+   ev.dataTransfer.setData("img",ev.target.id);
  }
+
+
+ document.addEventListener("dragover", function(event) {
+  event.preventDefault();
+});
 
  function drop(ev){
    ev.preventDefault();
-   var data = ev.dataTransfer.getData("icon");
    ev.target.appendChild(document.getElementById(data));
+ }
 
 //var audio = new Audio('audio/mixerBeat1.mp3'); audio.play();
 
-myAudio = new Audio('audio/mixerBeat1.mp3');
-myAudio.addEventListener('ended', function() {
-    this.currentTime = 0;
-    this.play();
-}, true);
-myAudio.play();
-
-myAudio = new Audio('audio/mixerBeat2.mp3');
-myAudio.addEventListener('ended', function() {
-    this.currentTime = 0;
-    this.play();
-}, true);
-myAudio.play();
-
-
  dropZones.forEach(zone => {
- zone.addEventListener("drag_Box", function(e) {
- e.preventDefault();
- console.log('dragged over')
+ zone.addEventListener("dragover", function(ev){
+ ev.preventDefault();
+ });
+ zone.addEventListener("drop", function(ev) {
 
- //if (this.childElementCount > 0) {return;}
- if(zone.childElementCount == 0) {
 
-   zone.classList.remove();
-   zone.classList.add("drag_Box");
+ ev.preventDefault(); // dont do your default behaviour instead do the following (console log)
+ console.log('you dropped something over me');
 
-   e.target.appendChild(document.querySelector(`#${draggedElement}`));
- }
- console.log('One Item Only');
+ if (this.childElementCount > 0) {
+   console.log('one child family, go away!');
+   return;
+}
+
+ myAudio = new Audio('audio/mixerBeat1.mp3');
+ myAudio = new Audio('audio/mixerBeat2.mp3');
+
+ myAudio.loop = "true"; document.body.appendChild(myAudio);
+ myAudio.play();
+ let data = ev.dataTransfer.getData("img");
+
+
+ ev.target.appendChild(document.querySelector(`#${data}`));
+ })
+
 });
 
-
- zone.addEventListener("drop", function(e){
- e.preventDefault(); // dont do your default behaviour instead do the following (console log)
- console.log('you dropped something over me')
- if (this.childElementCount > 0) {return; }
- console.log('one child family, go away!');
+})();
 
 
 
-});
-});
- }
+
+//debugger
